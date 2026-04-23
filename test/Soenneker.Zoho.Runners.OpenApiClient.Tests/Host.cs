46b7116f993484b1a7ccd2b150751c4f63c4fcab
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:46:08 -0500

    tunit

--- test/Soenneker.Zoho.Runners.OpenApiClient.Tests/Host.cs
+++ test/Soenneker.Zoho.Runners.OpenApiClient.Tests/Host.cs
@@ -1,14 +1,14 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Soenneker.Fixtures.Unit;
+using Soenneker.TestHosts.Unit;
 using Soenneker.Utils.Test;
 
 namespace Soenneker.Zoho.Runners.OpenApiClient.Tests;
 
-public sealed class Fixture : UnitFixture
+public sealed class Host : UnitTestHost
 {
-    public override System.Threading.Tasks.ValueTask InitializeAsync()
+    public override System.Threading.Tasks.Task InitializeAsync()
     {
         SetupIoC(Services);
 
--- test/Soenneker.Zoho.Runners.OpenApiClient.Tests/ZohoOpenApiClientRunnerTests.cs
+++ test/Soenneker.Zoho.Runners.OpenApiClient.Tests/ZohoOpenApiClientRunnerTests.cs
@@ -1,16 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Zoho.Runners.OpenApiClient.Tests;
 
-[Collection("Collection")]
-public sealed class ZohoOpenApiClientRunnerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class ZohoOpenApiClientRunnerTests : HostedUnitTest
 {
-    public ZohoOpenApiClientRunnerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ZohoOpenApiClientRunnerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
